a4c8062d3420bb75ca79ae1b3154edc124ae881c
chrswt <chriswt91@gmail.com>
2016-10-04 19:28:48 -0700

    Implement mongoDB in place of FS!

--- server/express.js
+++ server/express.js
@@ -2,18 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var multer = require('multer'); // v1.0.5
-var fs = require('fs');
+var mongodb = require('./mongodb');
 
 // Setup
 var storagePath = './server/storage.json';
 var app = express(); // actually creates HTTP server 
 var storage = {"results": []};
 var upload = multer(); // for parsing multipart/form-data
-
-// Load stored content into memory
-fs.readFile(storagePath, (err, data) => {
-  storage = JSON.parse(data.toString());
-});
 
 app.use(function(req, res, next) { // passes to all request methods
   res.header("Access-Control-Allow-Origin", "*");
@@ -34,14 +29,15 @@ app.get('/', function(req, res) {
 // Routing
 app.get('/classes/messages', function(req, res) { // 'next' parameter is like switch statement
   var status = 200;
-  var tempStorage = {};
-  tempStorage.results = storage.results.slice();
-  if (req.query.order) {
-    tempStorage.results.reverse();
+  var orderFlag = false;
+  if (req.query.order) { // TODO take care of query parameters properly
+    orderFlag = true;
   }
 
-  res.setHeader('Last-Modified', (new Date()).toUTCString()); // get 304 without this line
-  res.status(status).send(tempStorage); // don't have to stringify?
+  mongodb.getMessages(function(tempStorage) {
+    res.setHeader('Last-Modified', (new Date()).toUTCString()); // get 304 without this line
+    res.status(status).send(tempStorage); // don't have to stringify?
+  }, orderFlag);
 });
 
 app.post('/classes/messages', function(req, res) {
@@ -50,10 +46,8 @@ app.post('/classes/messages', function(req, res) {
   res.setHeader('Last-Modified', (new Date()).toUTCString()); // get 304 without this line
   var postReq = req.body;
   postReq.createdAt = new Date();
-  storage.results.push(postReq);
-  fs.writeFile(storagePath, JSON.stringify(storage), (err, data) => {
-  });
+  mongodb.postMessage(postReq);
   res.end(JSON.stringify({}));
 });
 
-app.listen(process.env.PORT);
+app.listen(3000);
---
+++ server/mongodb.js
@@ -0,0 +1,39 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/server';
+
+var postMessage = function(msgObject) {
+  MongoClient.connect(url, function(err, db) {
+    var messages = db.collection('messages');
+
+    messages.insertOne(msgObject, function(err, result) {
+      console.log('Successfully inserted ' + JSON.stringify(result) + ' into the Database.');
+    });
+
+    db.close();
+  });
+};
+
+var getMessages = function(callback, orderFlag) {
+  var tempStorage = {"results": []};
+  MongoClient.connect(url, function(err, db) {
+    var messages = db.collection('messages');
+    if (orderFlag) {
+      messages.find().sort({ createdAt: -1 }).toArray(function(err, result) { // TODO can also pass in sorting config directly from client
+        tempStorage.results = result;
+        callback(tempStorage);
+      });
+    } else {
+      messages.find().toArray(function(err, result) { // TODO can also pass in sorting config directly from client
+        tempStorage.results = result;
+        callback(tempStorage);
+      });
+    }
+
+    db.close();
+  });
+};
+
+module.exports.getMessages = getMessages;
+module.exports.postMessage = postMessage;
