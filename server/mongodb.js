2c4532e27f3850110b2c9f167ce129ae732fede2
Christopher Tham <chriswt91@gmail.com>
2016-10-04 23:25:38 -0700

    Implement mLab mongodb authentication

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,9 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-var url = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://heroku_np1v0g2w:hackreactor1@ds049446.mlab.com:49446/heroku_np1v0g2w';
+var url = 'mongodb://chris:hackreactor1@ds049446.mlab.com:49446/heroku_np1v0g2w';
 
 var postMessage = function(msgObject) {
   MongoClient.connect(url, function(err, db) {
@@ -20,6 +18,7 @@ var postMessage = function(msgObject) {
 var getMessages = function(callback, orderFlag) {
   var tempStorage = {"results": []};
   MongoClient.connect(url, function(err, db) {
+    if (err) { console.log (err); }
     var messages = db.collection('messages');
     if (orderFlag) {
       messages.find().sort({ createdAt: -1 }).toArray(function(err, result) { // TODO can also pass in sorting config directly from client
